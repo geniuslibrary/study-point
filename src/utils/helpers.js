663efcfb2c7e2269a377b578f44a667133e026d8
geniuslibrary <geniuslibrary1526@gmail.com>
2026-09-05 15:34:48 +0530

    Fix: Add type check to formatMonthDisplay to prevent crash

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -49,7 +49,7 @@ export const getMonthYear = (date = new Date()) => {
 };
 
 export const formatMonthDisplay = (yyyy_mm) => {
-  if (!yyyy_mm || !yyyy_mm.includes('-')) return yyy_mm;
+  if (!yyyy_mm || typeof yyy_mm !== 'string' || !yyyy_mm.includes('-')) return yyy_mm;
   const [year, month] = yyy_mm.split('-');
   return `${month}/${year}`;
 };
